4aa65e7483a970566cd54f513483b13f3232f79d
Deepak <deep.crazyman@gmail.com>
2013-03-30 23:54:33 +0530

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,27 @@
+
+<?php /*Custom CMS Project By : Deepak Mittal, Anmol Mittal, Ashish Maheshwari
+Completed on : 10-2-2013
+WT Based SE LAB ,VITS Gzb*/ ?>
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+
+<title>MADCMS</title>
+<!--Defining Stylesheet Of MADCMS-->
+<link href="http://localhost/MADCMS/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+<div id="sidebar">
+<p>I am a sidebar</p>
+<script language="JavaScript" type="text/javascript"> 
+ document.write('<script language="JavaScript" src="http://www.worldweatheronline.com/widget/v2/weather-widget.ashx?locid=99520&root_id=1&wc=FISHING&map=~/fishing/lac-transparent-weather-widget/quebec/ca.aspx&width=220&custom_header=MADCMS Weather Box&num_of_day=6&title_bg_color=020202&title_text_color=FFFFFF&widget_bg_color=FFFFFF&widget_text_color=020202&type=js&cb=' + Math.random() + '" type="text/javascript"><\/scr' + 'ipt>');
+ </script></div>
+<div id="wrapper">
+   
+   </body>
+   
+ </html>
